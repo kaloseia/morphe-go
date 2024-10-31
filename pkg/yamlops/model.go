e8049f976aea64d98a62d0ef219c48af70bdc4eb
bnunamak <brennan@nunamaker.de>
2024-10-31 10:00:15 +0100

    Added factored out model helper functions

---
+++ pkg/yamlops/model.go
@@ -0,0 +1,27 @@
+package yamlops
+
+import (
+	"fmt"
+
+	"github.com/kaloseia/morphe-go/pkg/yaml"
+)
+
+func getModelPrimaryIdentifierFieldName(modelDef yaml.Model) (string, error) {
+	primaryID, hasPrimaryID := modelDef.Identifiers["primary"]
+	if !hasPrimaryID {
+		return "", fmt.Errorf("model '%s' has no defined primary identifier", modelDef.Name)
+	}
+	if len(primaryID.Fields) != 1 {
+		return "", fmt.Errorf("model '%s' primary identifier has %v fields, 1 expected", modelDef.Name, len(primaryID.Fields))
+	}
+	primaryIDFieldName := primaryID.Fields[0]
+	return primaryIDFieldName, nil
+}
+
+func getModelFieldDefinitionByName(modelDef yaml.Model, fieldName string) (yaml.ModelField, error) {
+	fieldDef, hasField := modelDef.Fields[fieldName]
+	if !hasField {
+		return yaml.ModelField{}, fmt.Errorf("model '%s' has no defined field '%s'", modelDef.Name, fieldName)
+	}
+	return fieldDef, nil
+}
